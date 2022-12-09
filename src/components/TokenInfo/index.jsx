2e81748e0fc2b65522ae530ae0c2e924e735c5e3
João <jbsouzasantos1999@gmail.com>
2022-12-09 16:16:20 -0300

    push

--- src/components/TokenInfo/index.jsx
+++ src/components/TokenInfo/index.jsx
@@ -6,7 +6,7 @@ import presaleAbi from "../../contracts/presale.json";
 
 const TokenInfo = () => {
   const { userAddress, signer } = useContext(AppContext);
-  const presaleAddress = "0x1374d424b8b45d6E5F57a7Ba640a309eBF709c2A";
+  const presaleAddress = "0x6D7bd7D120c4BC3d8480d0ac29F337d35E6C5996";
 
   const [availableTokens, setAvailableTokens] = useState(0);
   const [balance, setBalance] = useState(0);
@@ -20,6 +20,7 @@ const TokenInfo = () => {
   useEffect(() => {
     getAvailableTokens();
     getUserBalance();
+    console.log(userAddress, balance);
   });
 
   const getAvailableTokens = async () => {
@@ -42,8 +43,7 @@ const TokenInfo = () => {
     );
 
     const available = await presaleContract.vestingRegistry(userAddress);
-    console.log(ethers.utils.formatEther(available._amount, 0));
-    setBalance(ethers.utils.formatEther(available._amount));
+    setBalance(Number(ethers.utils.formatEther(available._amount, 0)));
   };
 
   const claimTokens = async () => {
