52460f1da06c878bd7630c964c8f2849598fa17b
João <jbsouzasantos1999@gmail.com>
2022-12-20 15:50:50 -0300

    address

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { Web3Modal } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { bsc } from "wagmi/chains";
 
-const presaleAddress = "0x6D7bd7D120c4BC3d8480d0ac29F337d35E6C5996";
+const presaleAddress = "0xB95947a52D043A739515958B8638BA36709d6C71";
 
 function App() {
   const chains = [bsc];
